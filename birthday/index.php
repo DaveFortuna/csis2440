c3a9770c8ce66ed6e7dc678e39f00176e11da57e
David Fortuna <DavidFortuna639@gmail.com>
2025-01-16 15:17:43 -0700

    Birthday php csis2440 assignment

---
+++ birthday/index.php
@@ -0,0 +1,20 @@
+<?php
+  error_reporting(-1);
+  ini_set( 'display_errors', 1 );
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style href="css/style.css" type="text/css" rel="stylesheet"></style>
+    <script src="js/script.js"></script>
+
+    <title>Birthday</title>
+  </head>
+  <body>
+    <?php echo '<h1>Happy Birthday!</h1>';?>
+      <?php echo '<img src="img/catgrump.jpg">';?>
+
+
+  </body>
+</html>
