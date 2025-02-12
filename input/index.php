9a9f2d5c197f59d510704a6c8bcd19268259cb23
David Fortuna <DavidFortuna639@gmail.com>
2025-02-12 14:48:20 -0700

    Input assignment

--- input/index.php
+++ input/index.php
@@ -5,22 +5,40 @@
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <script src="js/function.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>User Input HIYAA!</title>
+  <title>User Input</title>
 
 </head>
 
 <body>
-  <form action="">
-    <label for="fname">First name:</label>
-    <input type="text" id="fname" name="fname"><br><br>
-    <label for="lname">Last name:</label>
-    <input type="text" id="lname" name="lname"><br><br>
-    <label for="num">Phone Number:</label>
-    <input type="number" id="num" name="num">
+  <main>
+    <?php
+  if(empty($_POST)){
+  
+   echo '<form method="post">';
 
-
-  </form>
-
+      echo '<label for="fname">First name: </label><br>';
+      echo '<input type="text" id="fname" name="fname"><br>';
+      echo '<label for="lname">Last name:</label><br>';
+      echo '<input type="text" id="lname" name="lname"><br>';
+      echo '<label for="num">Phone Number:</label><br>';
+      echo '<input type="tel" id="num" name="num"><br>';
+      echo '<label for="email">Email:</label><br>';
+      echo '<input type="email" id="email" name="email"><br><br>';
+      echo '<input class="button" type="submit" value="Submit">';
+    echo '</form>';
+  }
+  else
+  {
+    echo '<div class="response">';
+    echo '<h3>Thank you for your submission!</h3><br>';
+     echo '<p>Hello '.$_POST['fname'].' '.$_POST['lname'].'!</p><br>';
+     echo '<p>Your phone number is: '.$_POST['num'].'</p><br>';
+     echo '<p>Your E-mail address is: '.$_POST['email'].'</p>';
+    echo '</div>';
+  }
+?>
+  </main>
 </body>
+
 
 </html>
